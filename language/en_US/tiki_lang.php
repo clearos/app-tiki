a6c56de2fdc5530675c49cf77daa6d2cb41cb32f
Marc Laporte <marc@laporte.name>
2015-01-03 23:41:36 -0500

    Full name is better here.

--- language/en_US/tiki_lang.php
+++ language/en_US/tiki_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['tiki_app_name'] = 'Tiki Wiki CMS Groupware';
-$lang['tiki_app_description'] = 'Tiki is the Free / Libre / Open Source Web Application with the most built-in features.';
+$lang['tiki_app_description'] = 'Tiki Wiki CMS Groupware is the Free / Libre / Open Source Web Application with the most built-in features.';
 $lang['tiki_getting_started_message'] = 'Tiki has been initialized!  You can now proceed with the Installation Wizard to complete the process.  Please check the user guide for more tips and tricks.';
 
